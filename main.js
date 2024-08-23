2468a109d8439e8d5df02070ae70f6c11053ee7f
FizzaSalman <159646126+FizzaSalman@users.noreply.github.com>
2024-08-23 18:04:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Q no 3: Name Cases: Store a persons name in a variable,and then print that persons name in lower case,upper case and title case.
+Object.defineProperty(exports, "__esModule", { value: true });
+let person_name = "ahmedraza";
+//console.log(person_name.toLowerCase ())
+console.log(person_name.toUpperCase());
+//console.log(person_name.charAt(0).toUpperCase()+person_name.slice(1))
---
+++ main.ts
@@ -0,0 +1,7 @@
+//Q no 3: Name Cases: Store a persons name in a variable,and then print that persons name in lower case,upper case and title case.
+
+
+let person_name = "ahmedraza"
+//console.log(person_name.toLowerCase ())
+//console.log(person_name.toUpperCase())
+console.log(person_name.charAt(0).toUpperCase()+person_name.slice(1))
